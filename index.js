d24ee0d1253a3c976d9d8359df4d62569badc32d
Darrell Ford <Darrellford85@gmail.com>
2022-03-23 18:19:26 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+// Create a thank you card and allow them to send it
+const array = ['Guadalupe', 'Ollie', 'Aki']
+// const event = 'surprise'
+
+function writeCards(guest, events){
+    let reward = [];
+    for (let i = 0; i< guest.length; i++){
+      reward.push(`Thank you, ${guest[i]}, for the wonderful ${events} gift!`);
+    }
+    return reward;
+  }
+  
+  writeCards(guest, events)
+        // Runs console log and spy
+  function countDown(number){
+      let countDownNumber = number;
+      while (countDownNumber >= 0) {
+        console.log(countDownNumber);
+        countDownNumber = countDownNumber-1;
+    }
+}
+
+countDown(number)
+  
+        // spy-js to trace cmds and output//
